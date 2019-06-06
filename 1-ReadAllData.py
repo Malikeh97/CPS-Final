af6f3b528009b2c2c34e65ff1e9ebc0c6cb8bc8a
Malikeh97 <malikehehghaghi@gmail.com>
2019-06-06 14:48:34 +0430

    checking code implemented(RSSI mean diagram plotted)

--- 1-ReadAllData.py
+++ 1-ReadAllData.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Import libraries
-
-# In[1]:
-
 
 import pandas as pd
 from os import listdir
@@ -13,10 +7,6 @@ import numpy as np
 
 
 # # Reading the data files
-
-# In[2]:
-
-
 data_path = 'data/'
 all_data = []
 
@@ -33,33 +23,17 @@ df = pd.concat(all_data)
 # There may some unknow BLE devices which have been detected by our gateways and we should filter them.
 # Also, we have 2 data points for each tag: RSSI (Type=1) and TxPower (Type=2)
 
-# In[3]:
-
 df_tag_summary = df.groupby(['uuid']).count()
 df_tag_summary[:10]
 
 
-# In[4]:
-
 valid_tags = ['d6:fb:b2:5e:83:c5']
 data = df[df['uuid'].isin(valid_tags)]
 
-
-
-# # Changing 9.9m to 10.0m
-
-# In[6]:
-
-
 data.columns = ['uuid', 'distance', 'rssi']
-# data = data.drop_duplicates() #why?
-
-#data['Distance'] = data['Distance'].apply(lambda x: 0.03 if x == 0.0 else x)
 
 
 # # Saving data in a file
-
-# In[7]:
 
 print(data)
 data.to_csv('data.csv', index = False, header = data.columns)
--- 2-Checking.py
+++ 2-Checking.py
@@ -1,52 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Import libraries and loading data
-
-# In[1]:
-
-
+import gc
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+gc.enable()
+
 # read csv file
 data = pd.read_csv('data.csv')
 
-
 # # Checking collected data
 
-# ### How many data points were collected by each gateway (only RSSI)?
-
-# In[2]:
-
-
-df_gateway_summary = data.groupby(['Gateway']).size()#.sort_values(ascending=False)
-df_gateway_summary.plot(kind='bar', figsize=(16,8), grid = True, title = 'RSSI Count')
+#RSSI Mean
+df_gateway_summary = data.groupby(['distance']).mean()#.sort_values(ascending=False)
+df_gateway_summary.plot(kind='bar', figsize=(16,8), grid = True, title = 'RSSI Mean')
 plt.show()
-
-
-# ### How many data points were collected by each gateway (only RSSI) in each range?
-
-# In[3]:
-
-
-df_gateway_summary = data.groupby(['Gateway', 'Distance']).count()
-df_gateway_summary = df_gateway_summary['RSSI'].unstack()
-df_gateway_summary
-
-
-# In[4]:
-
-
-df_gateway_summary.plot(kind = 'bar', stacked = True, figsize=(16,8), title='RSSI Count', grid= True)
-plt.show()
-
-
-# In[5]:
-
-
-df_gateway_summary = data.groupby(['TagUid', 'Gateway', 'Distance', 'TxPower']).count()
-df_gateway_summary = df_gateway_summary['RSSI'].unstack()
-df_gateway_summary
-
